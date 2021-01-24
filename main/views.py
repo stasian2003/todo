0d744a2c4766f03d4961e25853d76ccaa85b7ee7
stasian2003 <stasian2003@gmail.com>
2021-01-24 23:42:35 +0600

    render book html in site

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import ToDo
+from .models import BShop
 
 def homepage(request):
     return render(request, 'index.html')
@@ -8,4 +9,9 @@ def test(request):
     todo_list = ToDo.objects.all()
 
     return render(request, 'test.html', {"todo_list":todo_list})
+
+def books(request):
+    book_list = BShop.objects.all()
+
+    return render(request, 'books.html', {"book_list":book_list})
 
--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test
+from main.views import homepage, test, books
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name='home'),
     path('test/', test, name='test'),
+    path('books/', books, name='book'),
    
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
