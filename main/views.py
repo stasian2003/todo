398acabf18295053a7410a45ac3a0685477a377d
stasian2003 <stasian2003@gmail.com>
2021-01-25 00:26:11 +0600

    added todo form

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo
 from .models import BShop
 
@@ -14,4 +14,11 @@ def books(request):
     book_list = BShop.objects.all()
 
     return render(request, 'books.html', {"book_list":book_list})
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
 
--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, books
+from main.views import homepage, test, books, add_todo
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', homepage, name='home'),
     path('test/', test, name='test'),
     path('books/', books, name='book'),
+    path('add-todo', add_todo, name="add-todo"),
    
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
